5aadfd98ed85c36046fff889cb15a22bd20e95cf
hennible0612 <sungwoo166@gmail.com>
2024-05-02 12:14:47 +0900

    Init

---
+++ src/main/java/com/mola/MolaTripApplication.java
@@ -0,0 +1,13 @@
+package com.mola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolaTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolaTripApplication.class, args);
+	}
+
+}
