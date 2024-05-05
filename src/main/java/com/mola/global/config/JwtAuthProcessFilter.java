8299e6737445e752c87a1a9735cfe0affca1af38
hennible0612 <sungwoo166@gmail.com>
2024-05-05 19:20:21 +0900

    Feat : Jwt 커스텀 필터 구현

---
+++ src/main/java/com/mola/global/config/JwtAuthProcessFilter.java
@@ -0,0 +1,44 @@
+package com.mola.global.config;
+
+
+import com.mola.global.security.service.JwtProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthProcessFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+
+            if (jwtProvider.verifyToken(token)) {
+                Long memberId = jwtProvider.extractMemberIdFromToken(token);
+                UserDetails user = jwtProvider.createUserDetails(memberId, "ROLE_USER");
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        user, "", user.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
