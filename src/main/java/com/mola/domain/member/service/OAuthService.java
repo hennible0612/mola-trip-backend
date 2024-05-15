01c00a824da915c47f538aa8efd061e417c7be52
hennible0612 <sungwoo166@gmail.com>
2024-05-15 17:54:13 +0900

    Chore : OAuth2 도입으로 인한 불필요 파일 제거

--- src/main/java/com/mola/domain/member/service/OAuthService.java
+++
@@ -1,8 +0,0 @@
-package com.mola.domain.member.service;
-
-import com.mola.domain.member.dto.OAuthMemberDto;
-
-public interface OAuthService {
-    String getOAuthToken(String authorizationCode);
-    OAuthMemberDto getOAuthUser(String accessToken);
-}
