58227437f236683572d9f48985b6c089df491bb9
hennible0612 <sungwoo166@gmail.com>
2024-05-15 17:56:04 +0900

    Feat : OAuth2 사용을 위한 핸들러, DTO 구현

--- src/main/java/com/mola/domain/member/entity/LoginProvider.java
+++ src/main/java/com/mola/domain/member/entity/LoginProvider.java
@@ -1,25 +1,15 @@
 package com.mola.domain.member.entity;
 
-import com.mola.domain.member.service.KakaoOAuthServiceImpl;
-import com.mola.domain.member.service.OAuthService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum LoginProvider {
-    KAKAO("kakao", KakaoOAuthServiceImpl.class);
+    KAKAO,
+    NAVER,
+    GOOGLE,
+    GITHUB;
 
-    private final String provider;
 
-    private final Class<? extends OAuthService> serviceClass;
-
-    public static LoginProvider toLoginProvider(String provider) {
-        for (LoginProvider loginProvider : values()) {
-            if (loginProvider.getProvider().equalsIgnoreCase(provider)) {
-                return loginProvider;
-            }
-        }
-        throw new IllegalArgumentException("No matching login provider found for: " + provider);
-    }
 }
--- src/main/java/com/mola/domain/member/entity/Member.java
+++ src/main/java/com/mola/domain/member/entity/Member.java
@@ -21,14 +21,18 @@ import lombok.Setter;
 @Builder
 @Entity
 public class Member {
+
     @Id
     @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
     @Column(name = "nickname", length = 100, nullable = false)
     private String nickname;
+
     @Column(name = "personal_id", nullable = false)
     private String personalId;
+
     @Column(name = "profile_image_url")
     private String profileImageUrl;
     @OneToMany(
@@ -36,10 +40,16 @@ public class Member {
             cascade = CascadeType.ALL
     )
     List<TripFriends> tripFriendsList;
+
     @Column(name = "refresh_token", length = 512)
     private String refreshToken;
+
+    @Column(name = "login_provider")
+    private LoginProvider loginProvider;
 
     public void updateRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
+
+
 }
