5c63beec7be3ddfb8efbb2e81e72bff31b1c6a69
hennible0612 <sungwoo166@gmail.com>
2024-05-17 01:16:12 +0900

    Feat : TripCode를 사용한 여행 참가 구현

--- src/main/java/com/mola/domain/trip/repository/TripPlanRepository.java
+++ src/main/java/com/mola/domain/trip/repository/TripPlanRepository.java
@@ -1,7 +1,9 @@
 package com.mola.domain.trip.repository;
 
 import com.mola.domain.trip.entity.TripPlan;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripPlanRepository extends JpaRepository<TripPlan, Long> {
+    Optional<TripPlan> findByTripCode(String tripCode);
 }
