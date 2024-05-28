ed5001b6000d79005cb48220cb0bbf5713c0b132
hennible0612 <sungwoo166@gmail.com>
2024-05-29 00:49:11 +0900

    Refactor : 불필요코드 삭제

--- src/main/java/com/mola/domain/member/service/MemberService.java
+++ src/main/java/com/mola/domain/member/service/MemberService.java
@@ -1,12 +1,9 @@
 package com.mola.domain.member.service;
 
-import com.mola.domain.member.dto.MemberActivityProfile;
 import com.mola.domain.member.dto.MemberResponseDto;
 import com.mola.domain.member.entity.Member;
 import com.mola.domain.member.entity.MemberRole;
 import com.mola.domain.member.repository.MemberRepository;
-import com.mola.domain.tripBoard.comment.entity.Comment;
-import com.mola.domain.tripBoard.comment.repository.CommentRepository;
 import com.mola.global.exception.CustomException;
 import com.mola.global.exception.GlobalErrorCode;
 import com.mola.global.util.SecurityUtil;
@@ -16,31 +13,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Service
 public class MemberService {
 
     private final SecurityUtil securityUtil;
 
-    private final CommentRepository commentRepository;
-
     private final MemberRepository memberRepository;
 
     @Value("${admin.secretKey}")
     private String secretKey;
-
-    public MemberActivityProfile getMemberActivity() {
-        Long memberId = securityUtil.findCurrentMemberId();
-
-        Optional<Comment> comments1 = commentRepository.findCommentsByMemberId(memberId);
-
-        Optional<Comment> comments2 = commentRepository.findCommentsByMemberIdJPQL(memberId);
-
-
-        return null;
-    }
 
     public Page<MemberResponseDto> getMembersForAdmin(Pageable pageable) {
         return memberRepository.findMembersForAdmin(pageable);
@@ -50,8 +32,8 @@ public class MemberService {
         memberRepository.deleteById(memberId);
     }
 
-    public Long requestAdmin(String key){
-        if(!key.equals(secretKey)){
+    public Long requestAdmin(String key) {
+        if (!key.equals(secretKey)) {
             throw new CustomException(GlobalErrorCode.AccessDenied);
         }
 
